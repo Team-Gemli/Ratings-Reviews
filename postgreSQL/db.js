67e694d2127af8dd76b7e2825b45b67e116ecb8e
Ermin Sljivo <esljivo1996@gmail.com>
2023-04-05 19:45:56 -0400

    fix db connection values

--- postgreSQL/db.js
+++ postgreSQL/db.js
@@ -3,8 +3,8 @@ const { Client, Pool } = require('pg');
 
 const pool = new Pool({
   user: 'postgres',
-  host: 'localhost',
-  database: 'ratingsandreviews',
+  host: process.env.HOST,
+  database: process.env.DATABASE,
   password: process.env.PASSWORD,
   port: 5432
 });
