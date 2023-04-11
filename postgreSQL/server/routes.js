78035a94b726bbb78a48da1e495c2d34632a51cb
Ermin Sljivo <esljivo1996@gmail.com>
2023-04-11 16:02:33 -0400

    use path

--- postgreSQL/server/routes.js
+++ postgreSQL/server/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const reviews = require('./controllers/reviews.js');
 const meta = require('./controllers/meta.js');
+const path = require('path');
 
 router.get('/reviews/', reviews.get) // done
 
@@ -16,7 +17,7 @@ router.put('/reviews/:review_id/helpful', reviews.helpfulness); // done
 router.put('/reviews/:review_id/report', reviews.report); //done
 
 router.get(`/${process.env.LOADERIO}`, (req, res) => {
-  res.sendFile('postgreSQL/loaderio-5408f80ebe356dd823daafefdd2c5a2b.txt');
+  res.sendFile(path.resolve(__dirname + 'loaderio-5408f80ebe356dd823daafefdd2c5a2b.txt');
 });
 
 module.exports.router = router;
